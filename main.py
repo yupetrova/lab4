5577620912dc5751e0d98d03be056895c0e00c94
Yuliia Petrova <yuliiatrova@gmail.com>
2024-02-15 00:04:56 +0200

    Add pow_five func
    Add pow_five func to utils.py and main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from utils import PrimeNum
+from utils import prime_num
 
 n = int(input())
-print(utils.PrimeNum(n))
+print(utils.prime_num(n))
+
+
+import math #request for pow_five
+from utils import pow_five
+
+print(utils.pow_five(n))
+
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 # функція для обчислення факторіалу
 
-def Factorial(n):
+def factorial(n):
     fact = 1
     if n < 0:
         raise ValueError("Please, input integer >= 0")
@@ -11,9 +11,10 @@ def Factorial(n):
             fact = fact * i
         print(fact)
 
+
 # чи є число простим
 
-def PrimeNum(n): #опис функції
+def prime_num(n): #опис функції
     is_prime = True
     for i in range (2, (n//2) + 1):
         if n % i == 0:
@@ -23,4 +24,20 @@ def PrimeNum(n): #опис функції
         print("Введене число є простим")
     else:
       print("Введене число НЕ є простим")
+
+
+#чи є число степенем 5
+
+import math
+def pow_five(n):
+    if n < 1 :
+        raise ValueError("Please, input integer >= 1")
+    elif n == 1:
+        return True
+    elif math.log(n, 5) % 1 == 0:
+            return True
+    else:
+        return False
+
+
 
